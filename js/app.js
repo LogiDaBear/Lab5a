61bf326cbd3aa4a5195a8300f5fa34129c113491
Logan Reese <lreese2112@gmail.com>
2023-02-10 22:32:38 +0000

    adds function to pass problemThree

--- js/app.js
+++ js/app.js
@@ -56,12 +56,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  
+  let sumAB = sum(a, b)[0];
+  let totalSum = sum(sumAB, c)[0];
+
+  let mulAB = multiply(a,b)[0];
+  let totalMul = multiply(mulAB, c)[0];
+
+  let thirdElem = `${a} and ${b} and ${c} sum to ${totalSum}.`;
+  let fourthElem = `The product of ${a} and ${b} and ${c} is ${totalMul}.`;
+
+  return [totalSum, totalMul, thirdElem, fourthElem];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -84,7 +93,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
